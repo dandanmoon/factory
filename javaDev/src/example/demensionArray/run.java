30666153e949290244a300e4261a09a5cc57cd1d
dandanmoon <answnsgh8691@gmail.com>
2021-03-29 14:43:03 +0900

    3/29

---
+++ javaDev/src/example/demensionArray/run.java
@@ -0,0 +1,9 @@
+package example.demensionArray;
+
+public class run {
+	public static void main(String[] args) {
+		DemensionArray de = new DemensionArray();
+		de.testDemensionArray();
+	}
+
+}
---
+++ javaDev/src/practice/demensionArray/run.java
@@ -0,0 +1,9 @@
+package practice.demensionArray;
+
+public class run {
+public static void main(String[] args) {
+	DemensionArrayPractice dp = new DemensionArrayPractice();
+	//dp.method1();
+	dp.method2();
+}
+}
